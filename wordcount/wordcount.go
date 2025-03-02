2b55c4c806e411a94b9f65f511908ee5867307d6
vladimir vivien <vladimir.vivien@gmail.com>
2025-03-02 23:03:57 +0000

    Fix to word count example

--- wordcount/wordcount.go
+++ wordcount/wordcount.go
@@ -2,71 +2,89 @@ package main
 
 import (
 	"bufio"
+	"cmp"
+	"context"
 	"fmt"
+	"log/slog"
 	"os"
 	"regexp"
+	"slices"
 	"strings"
 
-	"github.com/vladimirvivien/automi/collectors"
+	"github.com/vladimirvivien/automi/api/tuple"
+	"github.com/vladimirvivien/automi/operators/exec"
+	"github.com/vladimirvivien/automi/operators/flat"
+	"github.com/vladimirvivien/automi/operators/window"
+	"github.com/vladimirvivien/automi/sinks"
 	"github.com/vladimirvivien/automi/sources"
 	"github.com/vladimirvivien/automi/stream"
 )
 
 func main() {
-	// stream := stream.New([]string{
-	// 	"Hello World",
-	// 	"Hello Milkyway",
-	// 	"Hello Universe",
-	// })
+	regSpaces := regexp.MustCompile(`\s+`)
+	regNonAlpha := regexp.MustCompile(`[^a-zA-Z0-9\s]+`)
+	space := " "
 
-	f, err := os.Open("./twotw.txt")
+	// setup stream data source from file
+	file, err := os.Open("./twotw.txt")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	stream := stream.New(sources.Scanner(f, bufio.ScanLines))
 
-	regSpaces := regexp.MustCompile(`\s+`)
+	strm := stream.From(sources.Scanner(file, bufio.ScanLines))
+	strm.WithLogSink(sinks.SlogJSON(slog.LevelDebug))
+	strm.Run(
+		// normalize all spaces to single space, then return as slice of words
+		exec.Execute(func(_ context.Context, line []byte) []string {
+			return strings.Split(regSpaces.ReplaceAllLiteralString(string(line), space), space)
+		}),
 
-	// coerce incoming data to string
-	// then remove extranuous spaces.
-	stream.Process(func(line interface{}) string {
-		return regSpaces.ReplaceAllLiteralString(fmt.Sprintf("%s", line), " ")
-	})
+		// Flatten the slice of words into individual words
+		flat.Slice[[]string](),
 
-	// split lines into slice of words which
-	// are then streamed individually
-	stream.FlatMap(func(line string) []string {
-		return strings.Split(line, " ")
-	})
+		// Map each word to an occurence tuple of Pair{word, 1}
+		exec.Map(func(_ context.Context, word string) tuple.Pair[string, int] {
+			word = regNonAlpha.ReplaceAllLiteralString(word, "")
+			return tuple.Pair[string, int]{Val1: word, Val2: 1}
+		}),
 
-	// map word to an array where array[0]=word
-	// array[1]=1 to mark the occurence of word
-	stream.Map(func(word string) [2]interface{} {
-		return [2]interface{}{word, 1}
-	})
+		// Batch the occurence pairs into slices of []Pair{word, 1}
+		window.Batch[tuple.Pair[string, int]](),
 
-	// Next:
-	// 1) batch the stream of arrays from last step
-	// 2) group by position 0 which has the word which returns a map[word]count
-	// 3) Sum by the keys of the group
-	stream.Batch().GroupByPos(0).SumAllKeys()
+		// Group the occurences by word --> map[word][]Pair{word, 1}:
+		exec.GroupByStructField[[]tuple.Pair[string, int]]("Val1"),
 
-	// sink resunt to collector function
-	stream.Into(collectors.Func(func(items interface{}) error {
-		words := items.([]map[interface{}]float64)
-		for _, wmap := range words {
-			for word, count := range wmap {
-				fmt.Printf("%s:%0.0f\n", word, count)
+		// Count the occurences of each word from the group, return as []Pair{word, total}
+		exec.Execute(func(_ context.Context, group map[any][]tuple.Pair[string, int]) []tuple.Pair[string, int] {
+			var result []tuple.Pair[string, int]
+			for key, pairs := range group {
+				sum := 0
+				for _, pair := range pairs {
+					sum += pair.Val2
+				}
+				result = append(result, tuple.Pair[string, int]{Val1: key.(string), Val2: sum})
 			}
+
+			// sort result
+			slices.SortFunc(result, func(p1, p2 tuple.Pair[string, int]) int {
+				return cmp.Compare(p1.Val1, p2.Val1)
+			})
+
+			return result
+		}),
+	)
+
+	// Route the result to a sink that prints the word count
+	strm.Into(sinks.Func(func(wordp []tuple.Pair[string, int]) error {
+		for _, pair := range wordp {
+			fmt.Printf("%s: %d\n", pair.Val1, pair.Val2)
 		}
 		return nil
 	}))
 
-	// stream.SinkTo(collectors.Writer(os.Stdout))
-
 	// open the stream
-	if err := <-stream.Open(); err != nil {
+	if err := <-strm.Open(context.Background()); err != nil {
 		fmt.Println(err)
 		return
 	}
